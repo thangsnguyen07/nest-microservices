f3a63c01b9ba08e2db3502c02adec6507bd345bb
Thang Nguyen <thangsnguyen07@gmail.com>
2023-08-29 15:05:31 +0700

    feat: init user module

---
+++ services/user/.eslintrc.js
@@ -0,0 +1,38 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    tsconfigRootDir: __dirname,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint/eslint-plugin'],
+  extends: ['plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended', 'prettier'],
+  root: true,
+  env: {
+    node: true,
+    jest: true,
+  },
+  ignorePatterns: ['.eslintrc.js'],
+  rules: {
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unused-vars': ['error'],
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'prettier/prettier': [
+      'warn',
+      {
+        printWidth: 100,
+        proseWrap: 'always',
+        quoteProps: 'as-needed',
+        semi: false,
+        tabWidth: 2,
+        useTabs: false,
+        endOfLine: 'auto',
+        trailingComma: 'es5',
+      },
+    ],
+  },
+}
